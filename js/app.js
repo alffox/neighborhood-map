2d33537b8ea1329c2eb9d0b60b1339b2fc7f162b
alffox <alfonso.crisci.it@gmail.com>
2018-03-05 09:02:15 +0100

    Starter files and dummy map sample

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var map;
+
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.7413549,
+            lng: -73.9980244
+        },
+        zoom: 13
+    });
+    var tribeca = {
+        lat: 40.719526,
+        lng: -74.0089934
+    };
+    var marker = new google.maps.Marker({
+        position: tribeca,
+        map: map,
+        title: 'First Marker!'
+    });
+}
