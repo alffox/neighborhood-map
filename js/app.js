0b0f4413458faba3c81042a713b330eeadb7cf8d
alffox <alfonso.crisci.it@gmail.com>
2018-03-12 09:10:22 +0100

    feat: Reposition marker to Oyndarfjørður, Faroe Islands

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 40.7413549,
-            lng: -73.9980244
+            lat: 62.276329,
+            lng: -6.855419
         },
-        zoom: 13,
+        zoom: 10,
         scrollwheel: false,
         zoomControl: true
     });
     var tribeca = {
-        lat: 40.719526,
-        lng: -74.0089934
+        lat: 62.276329,
+        lng: -6.855419
     };
     var marker = new google.maps.Marker({
         position: tribeca,
